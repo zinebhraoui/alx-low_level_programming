bfb3562e6c29b6ecfa60a55b9de2b29417ee5eae
zineb hraoui <zineb>
2023-06-16 13:59:56 -0700

    task 6

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -6,10 +6,10 @@
 */
 int main(void)
 {
-	printf("Sice of a char: %d byte(s)\n", sizeof'(char));
-	printf("Sice of an int: %d byte(s)\n", sizeof'(int));
-	printf("Sice of a long int: %d byte(s)\n", sizeof'(long int));
-	printf("Sice of a long long int: %d byte(s)\n", sizeof'(long long int));
-	printf("Sice of a float: %d byte(s)\n", sizeof'(float));
-	return (0);
+        printf("Sice of a char: %d byte(s)\n", sizeof'(char));
+        printf("Sice of an int: %d byte(s)\n", sizeof'(int));
+        printf("Sice of a long int: %d byte(s)\n", sizeof'(long int));
+        printf("Sice of a long long int: %d byte(s)\n", sizeof'(long long int));
+        printf("Sice of a float: %d byte(s)\n", sizeof'(float));
+        return (0);
 }
