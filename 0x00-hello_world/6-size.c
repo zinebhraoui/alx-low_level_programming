dd10e8e20ee29ee914c0d8b86d62ba2c7ac1a2ee
zineb hraoui <zineb>
2023-06-16 13:42:56 -0700

    task 6

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+/**
+* main - Entry point
+*
+* Return: Always 0 (Success)
+*/
+int main(void)
+{
+	printf("Sice of a char: %d byte(s)\n", sizeof'(char));
+	printf("Sice of an int: %d byte(s)\n", sizeof'(int));
+	printf("Sice of a long int: %d byte(s)\n", sizeof'(long int));
+	printf("Sice of a long long int: %d byte(s)\n", sizeof'(long long int));
+	printf("Sice of a float: %d byte(s)\n", sizeof'(float));
+	return (0);
+}
