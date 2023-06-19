dd05093c1ad526ef7332c37088fbcd24b4b1997c
zineb hraoui <zineb>
2023-06-19 16:30:40 -0700

    task 7

--- 0x01-variables_if_else_while/7-print_tebahpla.c
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -4,10 +4,12 @@
 
 #include<stdio.h>
 
+
 /**
  * main - Entry point
  * Return: Always 0 (Success)
  */
+
 
 int main(void)
 
@@ -15,7 +17,7 @@ int main(void)
 	
 	char low;
 
-	for (low = 'z'; low >= 'a'; low -- )
+	for (low = 'z'; low >= 'a'; low-- )
 	putchar(low);
 	putchar('\n');
 
