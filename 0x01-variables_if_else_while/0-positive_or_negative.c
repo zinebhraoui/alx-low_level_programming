9c72d11f700acddfc5706fdc7814d1a51d3837cd
zineb hraoui <zineb>
2023-06-19 05:16:11 -0700

    task 0

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -14,7 +14,7 @@ int main(void)
 
 {
 	int n;
-	rand(time(0));
+	srand(time(0));
 	n = rand() -RAND_MAX / 2;
 
 	if (n > 0)
@@ -27,7 +27,7 @@ int main(void)
 	}
 	else
 	{
-	pritf("%d is negative\n", n);
+	printf("%d is negative\n", n);
 	}
 	return(0);
 }
