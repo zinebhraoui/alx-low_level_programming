55d5ee5aec6c9415f8e7414462f98f0c13f7aa45
zineb hraoui <zineb>
2023-06-19 05:34:34 -0700

    task 2

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,27 @@
+#include <stdlib.h>
+
+#include <time.h>
+
+#include <stdio.h>
+
+
+/**
+ * main - Entry point
+ * Retrn: Always 0 (Success)
+ */
+
+
+int main(void)
+
+{
+	char low;
+	for (low ='a'; low <= 'z'; low++)
+	putchar(low);
+
+	for(low = 'A'; low <= 'Z'; low++)
+	putchar(low);
+	putchar('\n');
+
+	return (0);
+
+}
