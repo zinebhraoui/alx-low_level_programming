2e62b0204d80cc97a0ade9754c942bf2c994c896
zineb hraoui <zineb>
2023-06-21 03:58:42 -0700

    task 0

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,13 +1,22 @@
-#include<unistd.h>
+#include "main.h"
 
 /**
- * writes the caracter c to stdout
- * Return:on Success 1.
+ * main - Entry point
+ * Description: 'writes the cracter c to stdout'
+ * Return: always 0
  */
 
-int _putchar(char c)
-
+int main(void)
 {
-	return (write(1, &c, 1));
+	_putchar('_');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
+	_putchar('\n');
+	return (0);
 
 }
