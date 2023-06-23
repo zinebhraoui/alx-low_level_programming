548d407f711e05ab3ff28e5cef512d9ab761c601
zineb hraoui <zineb>
2023-06-23 03:18:09 -0700

    task 1

--- 0x04-more_functions_nested_loops/1-isdigit.c
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -2,15 +2,15 @@
 
 /**
  * _isdigit -Check if a chracter is a digit
- * @x: The number to be checked
+ * @c: The number to be checked
  * Return: 1 for a character that will be digit or 0 for any else
  */
 
-int _isdigit(int x)
+int _isdigit(int c)
 
 {
 
-	if (x >= 48 && x <= 57=)
+	if (c >= 48 && c <= 57)
 	{
 	return (1);
 	}
