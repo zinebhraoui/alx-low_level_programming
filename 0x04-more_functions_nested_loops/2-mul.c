19835650deb358f94f76881e487b4e7c6b789577
zineb hraoui <zineb>
2023-06-23 03:26:30 -0700

    task 2

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * mul - mutiplies two integers
+ *
+ * @a: first input
+ * @b: second input
+ *
+ * Return: return results
+ */
+
+int mul(int a, int b)
+{
+	return (a * b);
+
+}
