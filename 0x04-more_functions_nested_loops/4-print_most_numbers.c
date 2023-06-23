28b9d91e8e85eb4252a58e6f7d9358f441c57d5c
zineb hraoui <zineb>
2023-06-23 04:45:09 -0700

    task 4

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -9,14 +9,14 @@
 
 void print_most_numbers(void)
 {
-        int num;
+	int num;
 
-        for (num = 0; num <= 9; num++)
-        {
+	for (num = 0; num <= 9; num++)
+	{
 		if (num == 2 || num == 4)
 			continue;
-		_putchar(num + 48);
-        }
-        _putchar('\n');
+	_putchar(num + 48);
+	}
+	_putchar('\n');
 
 }
