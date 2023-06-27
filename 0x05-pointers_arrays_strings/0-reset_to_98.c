cc0041c2c8d7debd912f3adbb25f94ca1f16b202
zineb hraoui <zineb>
2023-06-27 00:00:26 -0700

    task 0

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -4,7 +4,7 @@
  * reset_to_98 - takes a pointer to an int and updates
  * the value it points to 98
  *
- * @on: An integer input pointer
+ * @n: An integer input pointer
  *
  * Return : noting
  */
