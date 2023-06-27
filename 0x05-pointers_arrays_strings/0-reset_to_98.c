dc582eeb606be2648938809b763bcf66f65bb905
zineb hraoui <zineb>
2023-06-26 23:54:20 -0700

    task 0

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * reset_to_98 - takes a pointer to an int and updates
+ * the value it points to 98
+ *
+ * @on: An integer input pointer
+ *
+ * Return : noting
+ */
+void reset_to_98(int *n)
+{
+	*n = 98;
