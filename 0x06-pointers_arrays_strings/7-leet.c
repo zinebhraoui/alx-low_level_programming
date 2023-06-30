c1f5337436a9fd52e2940a4a888b8b7ae9b4bb8e
zineb hraoui <zineb>
2023-06-30 10:17:48 -0700

    task 7

--- 0x06-pointers_arrays_strings/7-leet.c
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -1,27 +1,27 @@
 #include "main.h"
+
 /**
  * cap_string - capitalizes all words of a string.
  * @s: the string to be capitalized.
- *
  * Return: String with all vowels changed.
  */
-char *leet(char *s)
-{
-	char a[] = "aeotlAEOTL";
-	char n[] = "4307143071";
-	int i = 0;
-	int j;
 
-	while (*(s + i) != '\0')
+char *leet(char *changed)
+{
+	int index, j;
+	char minus[] = {'a', 'e', 'o', 't', 'l', '\0'};
+	char mayus[] = {'A', 'E', 'O', 'T', 'L', '\0'};
+	char numbers[] = {'4', '3', '0', '7', '1', '\0'};
+
+	for (index = 0; changed[index] != '\0'; ++index)
 	{
-		for (j = 0; j <= 9; j++)
+		for (j = 0; j < 5; j++)
 		{
-			if (*(s + i) == a[j])
+			if (changed[index] == minus[j] || changed[index] == mayus[j])
 			{
-				*(s + i) = n[j];
+				changed[index] = numbers[j];
 			}
 		}
-		i++;
 	}
-	return (s);
+	return (changed);
 }
--- 0x06-pointers_arrays_strings/7-main.c
+++ 0x06-pointers_arrays_strings/7-main.c
@@ -15,6 +15,7 @@ char s[] = "Expect the best. Prepare for the worst. Capitalize on what comes.\n"
 char *p;
 
 p = leet(s);
+
 printf("%s", p);
 printf("%s", s);
 return (0);
