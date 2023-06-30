6d19f9e958255a39347197db4e1edd321a184aa1
zineb hraoui <zineb>
2023-06-30 09:22:52 -0700

    task 4

--- 0x06-pointers_arrays_strings/4-rev_array.c
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -14,7 +14,7 @@ void reverse_array(int *a, int n)
 
 	for (index = n - 1; index >= n / 2; index--)
 	{
-	tmp = a[n - 1 - index] = a[index];
+	tmp = a[n - 1 - index];
 	a[n - 1 - index] = a[index];
 	a[index] = tmp;
 	}
